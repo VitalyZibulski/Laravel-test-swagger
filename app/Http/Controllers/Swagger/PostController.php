e955a5b374d00549b28741d222ab219ba7e89992
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2023-06-01 12:18:03 +0300

    add crud annotations

--- app/Http/Controllers/Swagger/PostController.php
+++ app/Http/Controllers/Swagger/PostController.php
@@ -31,7 +31,108 @@ use App\Http\Controllers\Controller;
  *             )
  *          ),
  *     )
- * )
+ * ),
+ *
+ * @OA\Get(
+ *     path="/api/posts",
+ *     summary="List posts",
+ *     tags={"Posts"},
+ *
+ *     @OA\Response(
+ *          response=200,
+ *          description="Ok",
+ *          @OA\JsonContent(
+ *                  @OA\Property(property="data", type="array", @OA\Items(
+ *                      @OA\Property(property="id", type="integer", example=1),
+ *                      @OA\Property(property="title", type="string", example="Some title"),
+ *                      @OA\Property(property="likes", type="integer", example=10),
+ *                  )),
+ *             )
+ *          ),
+ *     )
+ * ),
+ *
+ * @OA\Get(
+ *     path="/api/posts/{post}",
+ *     summary="Show post",
+ *     tags={"Posts"},
+ *     @OA\Parameter(
+ *         description="Post id",
+ *         in="path",
+ *         name="post",
+ *         required=true,
+ *         example=1
+ *     ),
+ *     @OA\Response(
+ *          response=200,
+ *          description="Ok",
+ *          @OA\JsonContent(
+ *                  @OA\Property(property="data", type="object",
+ *                  @OA\Property(property="id", type="integer", example=1),
+ *                  @OA\Property(property="title", type="string", example="Some title"),
+ *                  @OA\Property(property="likes", type="integer", example=10),
+ *             )
+ *          ),
+ *     )
+ * ),
+ *
+ * @OA\Patch(
+ *     path="/api/posts/{post}",
+ *     summary="Update post",
+ *     tags={"Posts"},
+ *
+ *     @OA\Parameter(
+ *         description="Post id",
+ *         in="path",
+ *         name="post",
+ *         required=true,
+ *         example=2
+ *     ),
+ *
+ *     @OA\RequestBody(
+ *        @OA\JsonContent(
+ *          allOf={
+ *              @OA\Schema(
+ *                  @OA\Property(property="title", type="string", example="Some title for edit"),
+ *                  @OA\Property(property="likes", type="integer", example=10),
+ *              )
+ *          }
+ *        )
+ *     ),
+ *
+ *     @OA\Response(
+ *          response=200,
+ *          description="Ok",
+ *          @OA\JsonContent(
+ *                  @OA\Property(property="data", type="object",
+ *                  @OA\Property(property="id", type="integer", example=1),
+ *                  @OA\Property(property="title", type="string", example="Some title"),
+ *                  @OA\Property(property="likes", type="integer", example=10),
+ *             )
+ *          ),
+ *     )
+ * ),
+ *
+ * @OA\Delete(
+ *     path="/api/posts/{post}",
+ *     summary="Delete post",
+ *     tags={"Posts"},
+ *     @OA\Parameter(
+ *         description="Post id",
+ *         in="path",
+ *         name="post",
+ *         required=true,
+ *         example=1
+ *     ),
+ *     @OA\Response(
+ *          response=200,
+ *          description="Ok",
+ *          @OA\JsonContent(
+ *                  @OA\Property(property="message", type="string", example="done"),
+ *             )
+ *          ),
+ *     )
+ * ),
  *
  */
 class PostController extends Controller
